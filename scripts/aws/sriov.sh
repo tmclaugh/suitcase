1a83c4f18dd56fbf699576b0473c608390f36e1b
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-01-28 09:55:47 -0500

    Only install ixgbe* drivers on CentOS 6.x

--- scripts/aws/sriov.sh
+++ scripts/aws/sriov.sh
@@ -2,8 +2,10 @@
 #
 # Download and install updated NIC drivers for SR-IOV.
 
-cd ~/
-wget http://elrepo.org/linux/elrepo/el6/x86_64/RPMS/kmod-ixgbe-3.23.2-1.el6.elrepo.x86_64.rpm
-wget http://elrepo.org/linux/elrepo/el6/x86_64/RPMS/kmod-ixgbevf-2.14.2-1.el6.elrepo.x86_64.rpm
+if [ "${os_version::1}" = "6" ]; then
+    cd ~/
+    wget http://elrepo.org/linux/elrepo/el6/x86_64/RPMS/kmod-ixgbe-3.23.2-1.el6.elrepo.x86_64.rpm
+    wget http://elrepo.org/linux/elrepo/el6/x86_64/RPMS/kmod-ixgbevf-2.14.2-1.el6.elrepo.x86_64.rpm
 
-rpm -Uvh kmod-ixgbe*
+    rpm -Uvh kmod-ixgbe*
+fi
