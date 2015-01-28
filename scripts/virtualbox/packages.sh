2908a2b1d444e7b51957b507f0799aac2336feee
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-01-28 10:17:29 -0500

    Add FIXME to review this again later

--- scripts/virtualbox/packages.sh
+++ scripts/virtualbox/packages.sh
@@ -2,6 +2,8 @@
 #
 
 # A few packages
+# FIXME: I don't think these are needed anymore.  Packages are installed in
+# kickstart or don't appear to be neccesary any longer.
 yum install -y openssh-ldap
 yum install -y ruby-devel
 yum install -y rubygems
