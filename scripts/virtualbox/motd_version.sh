44fdf47a901a4c4a478a4e09ac672a772f251652
Tom McLaughlin <tom@jana.com>
2015-03-01 00:43:13 -0500

    really make login screen easier to read

--- scripts/virtualbox/motd_version.sh
+++ scripts/virtualbox/motd_version.sh
@@ -4,14 +4,16 @@
 
 # Image versioning Info
 
-echo "      _                             _"        > /etc/motd
+echo "" > /etc/motd
+echo "      _                             _"        >> /etc/motd
 echo "  ___| |_ _ __ __ _ _   _  ___ __ _| |_"      >> /etc/motd
 echo " / __| __| '__/ _, | | | |/ __/ _, | __|"     >> /etc/motd
 echo " \__ \ |_| | | (_| | |_| | (_| (_| | |_"      >> /etc/motd
 echo " |___/\__|_|  \__,_|\__, |\___\__,_|\__|"     >> /etc/motd
 echo "                    |___/"                    >> /etc/motd
 echo " (base image, $vm_name / $kickstart)"         >> /etc/motd
-
+echo "" >> /etc/motd
+echo "" >> /etc/motd
 
 cat >> /etc/straycat-base-image << EOF
 
