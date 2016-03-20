c1df0656f913810ca769173b8fe6d03de0293e5c
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-03-20 16:51:54 -0400

    Update method for doing OS version checks.

--- tests/spec/aws-hvm/grub_spec.rb
+++ tests/spec/aws-hvm/grub_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 console="console=ttyS0,115200n8 console=tty0"
 
-if os[:family] == 'RedHat7'
+if os[:family] == 'redhat' and os[:release] >= '7'
   describe file('/etc/default/grub') do
     its(:content) { should match /GRUB_CMDLINE_LINUX.*#{console}/ }
   end
