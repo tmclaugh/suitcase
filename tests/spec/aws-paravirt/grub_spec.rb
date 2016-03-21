3c0b89787a3d5ad4e34d8416370d8f9faf9bff66
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-03-20 20:36:39 -0400

    Fix tests on CentOS 6

--- tests/spec/aws-paravirt/grub_spec.rb
+++ tests/spec/aws-paravirt/grub_spec.rb
@@ -6,7 +6,7 @@ describe file('/boot/grub/menu.lst') do
   it { should be_linked_to '/boot/grub/grub.conf' }
   its(:content) { should match /kernel.*console=hvc0/ }
   its(:content) { should match /root \(hd0\)/ }
-  if os[:family] == 'redhat' and os[:release] <= '6'
+  if os[:family] == 'redhat' and os[:release] < '7'
     it { should contain /kernel.* xen_blkfront.sda_is_xvda=1/}
   end
 end
