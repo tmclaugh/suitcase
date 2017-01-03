168297bd8be60fe58107969305aa23599f006c12
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-01-02 20:29:47 -0500

    Handle DNS wonkiness. :-/

--- scripts/virtualbox/dns.sh
+++ scripts/virtualbox/dns.sh
@@ -1,13 +1,17 @@
 #!/bin/sh -x
 
+# NOTE: NetworkManager will override this so see below.
 # Set to publicly accesible nameserver
-/bin/echo nameserver 8.8.8.8 > /etc/resolv.conf
+#/bin/echo nameserver 8.8.8.8 > /etc/resolv.conf
 
 # Make sure that DNS does not get updated by AWS.
 sed -i -e 's/\(PEERDNS\)=yes/\1=no/' /etc/sysconfig/network-scripts/ifcfg-*
-# Safety in case for some reason the installer doesn't set this.
+
 for _nic in $(find /etc/sysconfig/network-scripts/ifcfg-* ! -name ifcfg-lo); do
     if ! egrep ^PEERDNS $_nic; then
         echo "PEERDNS=no" >> $_nic
+    fi
+    if ! egrep ^DNS1 $_nic; then
+        echo "DNS1=8.8.8.8" >> $_nic
     fi
 done
